7101f140967548c5ae012c87c9c8c965493ef5d3
Josie Chen <josieyxchen@gmail.com>
2020-11-20 16:14:11 -0500

    feat: successfully processed dummy data and generated labels

--- src/Globe.js
+++ src/Globe.js
@@ -1,27 +1,77 @@
-import React, { useRef }from 'react'
-// Canvas creates a scene and camera
-// useFrame renders animations
-import { useFrame, useLoader } from 'react-three-fiber'
-import { softShadows } from 'drei'
-import * as THREE from 'three'
-// there are two ways to access an image, if in the same folder as the current js file, then import; if in public fold, then process.env.PUBLIC_URL + '/earth.jpg'
-import map from './globeNight.jpg'
+// useEffect is used for data calls
+import React, {useState, useEffect, useRef} from 'react'
+import Globe from 'react-globe.gl'
+import nightView from './globeNight.jpg'
+import starField from './starField.png'
 
-softShadows()
+const dummyData =[
+  {
+    "name": "Philadelphia",
+    "country": "USA",
+    "lat": 39.95,
+    "lon": -75.16,
+    "years": 10
+  },
+  {
+    "name": "New York City",
+    "country": "USA",
+    "lat": 40.71,
+    "lon": -74.00,
+    "years": 0.1
+  },
+  {
+    "name": "Chicago",
+    "country": "USA",
+    "lat": 41.87,
+    "lon": -87.62,
+    "years": 2
+  },
+  {
+    "name": "Beijing",
+    "country": "China",
+    "lat": 39.90,
+    "lon": 116.40,
+    "years": 4
+  }
+];
 
-const Globe = ({ position, color, args}) => {
-  // make mesh available as a ref in the life cycle of this component
-  const mesh = useRef();
-  useFrame(()=> (mesh.current.rotation.y += 0.01))
-  const texture = useLoader(THREE.TextureLoader, map)
+const routes = {}
+// change pop_max to duration
+// label altitude ranges from 0.1 - 1.1
+// label size and dot radius range from 0.5 - 1.5
 
-  return (
-    <mesh
-      position={position}
-      ref={mesh}>
-      <sphereBufferGeometry attach='geometry' args={args} />
-      <meshPhongMaterial color={color} attach='material' map={texture} />
-    </mesh>)
+const Places = () => {
+
+  // const [places, setPlaces] = useState([]);
+  // setPlaces([dummyData])
+  // console.log(places);
+
+  return <Globe
+    globeImageUrl={nightView}
+    waitForGlobeReady={true}
+    backgroundImageUrl={starField}
+
+    labelsData={dummyData}
+    labelLat={(d)=> d.lat}
+    labelLng={(d)=> d.lon}
+    labelText={(d)=> d.name}
+    labelAltitude={(d) => {
+      if (d.years > 20) return 1.1;
+      return 0.1 + Math.floor(d.years / 2) / 10;
+    }}
+    labelSize= {(d) => {
+      if(d.years > 20) return 1.5
+      return 0.5 + Math.floor(d.years / 2) /10
+    }}
+    labelDotRadius={(d) => {
+      if(d.years > 20) return 1.5
+      return 0.5 + Math.floor(d.years / 2) /10
+    }}
+    labelColor={() => 'rgba(255, 165, 0, 0.75)'}
+    labelResolution={5}
+
+
+  />
 }
 
-export default Globe;
+export default Places;
