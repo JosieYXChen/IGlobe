c591b9d2608203863d8d79d0cb09343d84f6362c
Josie Chen <josieyxchen@gmail.com>
2020-11-20 13:30:11 -0500

    created a Globe with react-globe library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
-import Globe from './components/Globe'
+// Canvas creates a scene and camera
+import React, { Suspense } from 'react'
+// import { Canvas } from 'react-three-fiber'
+// import { OrbitControls } from 'drei'
+// import Cube from './Cube'
+import PlainPlaces from './PlainPlaces'
+// const Globe = React.lazy(() => import('./Globe'))
+const Places = React.lazy(()=> import('./Places'))
+//what the fallback return should be another three.js model
 
 function App() {
   return (
-    <Globe />
+    <Suspense fallback={<PlainPlaces />}>
+      <Places />
+    </Suspense>
+      // <Canvas
+      //   colorManagement
+      //   shadowMap
+      //   camera={{ position: [-5, 2, 10], fov: 60}}>
+      //   <ambientLight intensity={8.0} />
+      //   {/* <directionalLight
+      //     position={[-10, 0, -30]}
+      //     angle={100}
+      //     intensity={2.0}
+      //   /> */}
+      //     <Suspense fallback={<Cube
+      //         position={[0, 0, 0]}
+      //         color='lightblue'
+      //         args={[4.5, 100, 100]}/>}>
+      //       <Globe
+      //         position={[0, 0, 0]}
+      //         color='lightblue'
+      //         args={[4.5, 100, 100]}
+      //       />
+      //     </Suspense>
+      //   <OrbitControls />
+      // </Canvas>
   );
 }
 
---
+++ src/PlainPlaces.js
@@ -0,0 +1,7 @@
+import React from "react"
+import Globe from 'react-globe.gl'
+// import starField from './starField.png'
+
+const PlainPlaces = () => <Globe />
+
+export default PlainPlaces;
