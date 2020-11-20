df4b37f915555bb206056c608f24cfbe3ca74235
Josie Chen <josieyxchen@gmail.com>
2020-11-20 13:32:52 -0500

    created a globe via react-three-fiber

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
 import './App.css';
 // Canvas creates a scene and camera
 import React, { Suspense } from 'react'
-// import { Canvas } from 'react-three-fiber'
-// import { OrbitControls } from 'drei'
-// import Cube from './Cube'
-import PlainPlaces from './PlainPlaces'
-// const Globe = React.lazy(() => import('./Globe'))
-const Places = React.lazy(()=> import('./Places'))
+import { Canvas } from 'react-three-fiber'
+import { OrbitControls } from 'drei'
+import Cube from './Cube'
+// import PlainPlaces from './PlainPlaces'
+const Globe = React.lazy(() => import('./Globe'))
+// const Places = React.lazy(()=> import('./Places'))
 //what the fallback return should be another three.js model
 
 function App() {
   return (
-    <Suspense fallback={<PlainPlaces />}>
-      <Places />
-    </Suspense>
-      // <Canvas
-      //   colorManagement
-      //   shadowMap
-      //   camera={{ position: [-5, 2, 10], fov: 60}}>
-      //   <ambientLight intensity={8.0} />
-      //   {/* <directionalLight
-      //     position={[-10, 0, -30]}
-      //     angle={100}
-      //     intensity={2.0}
-      //   /> */}
-      //     <Suspense fallback={<Cube
-      //         position={[0, 0, 0]}
-      //         color='lightblue'
-      //         args={[4.5, 100, 100]}/>}>
-      //       <Globe
-      //         position={[0, 0, 0]}
-      //         color='lightblue'
-      //         args={[4.5, 100, 100]}
-      //       />
-      //     </Suspense>
-      //   <OrbitControls />
-      // </Canvas>
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{ position: [-5, 2, 10], fov: 60}}>
+        <ambientLight intensity={8.0} />
+        {/* <directionalLight
+          position={[-10, 0, -30]}
+          angle={100}
+          intensity={2.0}
+        /> */}
+          <Suspense fallback={<Cube
+              position={[0, 0, 0]}
+              color='lightblue'
+              args={[4.5, 100, 100]}/>}>
+            <Globe
+              position={[0, 0, 0]}
+              color='lightblue'
+              args={[4.5, 100, 100]}
+            />
+          </Suspense>
+        <OrbitControls />
+      </Canvas>
   );
 }
 
