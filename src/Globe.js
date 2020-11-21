045a76e0af93592cbd7e46736b981848a45ac1d9
Josie Chen <josieyxchen@gmail.com>
2020-11-21 12:41:43 -0500

    successfully fetched data from firebase realtime database

--- src/Globe.js
+++ src/Globe.js
@@ -3,55 +3,99 @@ import React, {useState, useEffect, useRef} from 'react'
 import Globe from 'react-globe.gl'
 import nightView from './globeNight.jpg'
 import starField from './starField.png'
+import firebase from 'firebase';
 
-const dummyData =[
-  {
-    "name": "Philadelphia",
-    "country": "USA",
-    "lat": 39.95,
-    "lon": -75.16,
-    "years": 10
-  },
-  {
-    "name": "New York City",
-    "country": "USA",
-    "lat": 40.71,
-    "lon": -74.00,
-    "years": 0.1
-  },
-  {
-    "name": "Chicago",
-    "country": "USA",
-    "lat": 41.87,
-    "lon": -87.62,
-    "years": 2
-  },
-  {
-    "name": "Beijing",
-    "country": "China",
-    "lat": 39.90,
-    "lon": 116.40,
-    "years": 4
-  }
-];
 
-const routes = {}
+const MAP_CENTER = {
+  lat: 39.95,
+  lng: -75.16,
+  altitude: 2.5
+}
+
+// const dummyData =[
+//   {
+//     "name": "Philadelphia",
+//     "country": "USA",
+//     "lat": 39.95,
+//     "lon": -75.16,
+//     "years": 10
+//   },
+//   {
+//     "name": "New York City",
+//     "country": "USA",
+//     "lat": 40.71,
+//     "lon": -74.00,
+//     "years": 0.1
+//   },
+//   {
+//     "name": "Chicago",
+//     "country": "USA",
+//     "lat": 41.87,
+//     "lon": -87.62,
+//     "years": 2
+//   },
+//   {
+//     "name": "Beijing",
+//     "country": "China",
+//     "lat": 39.90,
+//     "lon": 116.40,
+//     "years": 4
+//   }
+// ];
+
+// const dummyRoutes = [
+//   {
+//     "start": {
+//       "lat": 39.95,
+//       "lon": -75.16,
+//     },
+//     "end": {
+//       "lat": 39.90,
+//       "lon": 116.40,
+//     }
+//   },
+// ]
 // change pop_max to duration
 // label altitude ranges from 0.1 - 1.1
 // label size and dot radius range from 0.5 - 1.5
 
-const Places = () => {
 
-  // const [places, setPlaces] = useState([]);
+const Places = () => {
+  const globeEl = useRef();
+  const [places, setPlaces] = useState([]);
   // setPlaces([dummyData])
   // console.log(places);
 
-  return <Globe
-    globeImageUrl={nightView}
-    waitForGlobeReady={true}
-    backgroundImageUrl={starField}
+  useEffect(() => {
+    const rootRef = firebase.database().ref();
+    rootRef.on('value', async (snapshot) => {
+      const data = await snapshot.val().places
+      setPlaces(data);
+    }, error => console.log(error))
+    globeEl.current.pointOfView(MAP_CENTER, 4000)
+  }, [])
 
-    labelsData={dummyData}
+  return <Globe
+    ref={globeEl}
+    backgroundImageUrl={starField}
+    animateIn={true}
+    globeImageUrl={nightView}
+
+    // arcsData={dummyRoutes}
+    // arcStartLat={d => {
+    //   console.log(d.start.lat);
+    //   return d.start.lat
+    // }}
+    // arcStartLng={d => d.start.lng}
+    // arcEndLat={d => d.end.lat}
+    // arcEndLng={d => d.end.lng}
+    // arcDashLength={() => 0.4}
+    // arcDashGap={() => 0.2}
+    // arcDashAnimateTime={() => 1500}
+    // arcsTransitionDuration={1000}
+    // arcColor={() => 'rgba(255, 165, 0, 0.75)'}
+
+    labelsData={places}
     labelLat={(d)=> d.lat}
     labelLng={(d)=> d.lon}
     labelText={(d)=> d.name}
@@ -69,8 +113,6 @@ const Places = () => {
     }}
     labelColor={() => 'rgba(255, 165, 0, 0.75)'}
     labelResolution={5}
-
-
   />
 }
 
