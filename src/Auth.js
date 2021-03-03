204ab6bea9006007c726813579e27ba1ff53d22b
Josie Chen <josieyxchen@gmail.com>
2021-03-03 17:02:12 -0500

    Add nav bar to auth component
    Style auth component

--- src/Auth.js
+++ src/Auth.js
@@ -2,14 +2,19 @@ import React, { useEffect, useState } from 'react';
 import FirebaseAuth from 'react-firebaseui/FirebaseAuth';
 import firebase from 'firebase';
 import App from './App';
-
+import Nav from './Nav';
+import './Auth.css'
+import './firebaseui-styling.global.css'
 
 const uiConfig = {
   signInFlow: 'popup',
   signInSuccessUrl: '/app',
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
   ],
 };
 
@@ -24,7 +29,8 @@ const Auth = () => {
   }, []);
 
   if (!isSignedIn) {
-      return <div id="auth-container">
+      return <div className="auth-container">
+        <Nav />
         <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
       </div>
   }
@@ -32,3 +38,5 @@ const Auth = () => {
 };
 
 export default Auth;
+
+
